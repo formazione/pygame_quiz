12bb808a4da057a93384a9b2efb75977d8e4d169
formazione <gatto.gio@gmail.com>
2021-11-01 15:08:19 +0100

    new commit

--- pyquiz5.py
+++ pyquiz5.py
@@ -14,6 +14,7 @@ screen = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
 buttons = pygame.sprite.Group()
 num = 1
+
 class Button(pygame.sprite.Sprite):
     ''' A button treated like a Sprite... and killed too '''
     
@@ -125,12 +126,6 @@ class Button(pygame.sprite.Sprite):
 def on_click():
     print("Click on one answer")
 
-def on_run():
-    print("Ciao bello questo è RUN")
-
-def on_save():
-    print("This is Save")
-
 def on_right():
     check_score("right")
 
@@ -143,31 +138,34 @@ def check_score(answered="wrong"):
     global qnum, points
     
     # until there are questions (before last)
+    hit.play() # click sound
     if qnum < len(questions):
         print(qnum, len(questions))
-        hit.play() # click sound
         if answered == "right":
             time.sleep(.1) # to avoid adding more point when pressing too much
             points += 1
             # Show the score text
-            qnum += 1 # counter for next question in the list
-            score.change_text(str(points))
-            # Change the text of the question
-            title.change_text(questions[qnum-1][0], color="cyan")
-            # change the question number
-            num_question.change_text(str(qnum))
-            show_question(qnum) # delete old buttons and show new
+        qnum += 1 # counter for next question in the list
+        score.change_text(str(points))
+        # Change the text of the question
+        title.change_text(questions[qnum-1][0], color="cyan")
+        # change the question number
+        num_question.change_text(str(qnum))
+        show_question(qnum) # delete old buttons and show new
+        
+
     # for the last question...
     elif qnum == len(questions):
         print(qnum, len(questions))
-        hit.play() # click sound
         if answered == "right":
+            kill()
             time.sleep(.1)
             points +=1
-            score.change_text("You reached a score of " + str(points))
-            time.sleep(1)
-            kill()
-            start_again()
+        score.change_text("You reached a score of " + str(points))
+    time.sleep(.5)
+
+
+
 
 
 questions = [
